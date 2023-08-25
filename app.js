26e1499975445f14c1b354366bda6ad54c80d373
Arjun <arj.kv42@gmail.com>
2023-08-25 16:07:52 +0530

    Base Routes Added

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
+import router from "./routes/baseRoutes.js";
+
 
 const app = express();
 const PORT = 4000;
+app.use('/',router)
 app.listen(PORT, () => {
     mongoose
       .connect("mongodb+srv://42arju:arju123@cluster0.zgj4mct.mongodb.net/", {
